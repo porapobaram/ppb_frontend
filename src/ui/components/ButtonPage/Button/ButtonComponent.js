7d2ce9d30780462fcbddd1dc913dcaaae67b70a2
Nickolay Kuzmyn <kuzmin.study@gmail.com>
2020-01-25 14:07:20 +0200

    create branch, add component

---
+++ src/ui/components/ButtonPage/Button/ButtonComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ButtonComponent = ({}) => {
+  return (
+    <div>
+      {/* <FbLoginBtn onClick={} /> */}
+    </div>
+  );
+};
+
+export default ButtonComponent;
---
+++ src/ui/components/ButtonPage/Button/ButtonContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ButtonComponent from "./ButtonComponent";
+// Class HOC, which returns modificated component
+
+class ButtonContainer extends Component {
+  constructor(props) {
+    super(props);
+    // this.onCallbackHandler = this.onCallbackHandler.bind(this);
+  }
+}
+
+export default ButtonContainer;
