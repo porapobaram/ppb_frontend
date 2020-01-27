17aa3f0cfd26ab8f019765bcc4c70f93276b1542
NataliyaPetruk <48244809+NataliyaPetruk@users.noreply.github.com>
2020-01-28 01:37:59 +0200

    added dependencies and render

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
-import Login from "./ui/Login";
+import BarCrawlPage from "./ui/components/BarCrawlPage";
 
 const store = configureStore({});
 
 ReactDOM.render(
   <Provider store={store}>
+    <BarCrawlPage />
   </Provider>,
   document.getElementById("root")
 );
