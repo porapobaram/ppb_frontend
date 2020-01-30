0b41a022185911f9778333bd2c2218f0999e9a42
Nickolay Kuzmyn <kuzmin.study@gmail.com>
2020-01-30 19:04:00 +0200

    pull 30.01.20

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                {/*header*/}
-                {/*start header example*/}
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/randomizer">randomizer</Link>
-                    </li>
-                    <li>
-                        <Link to="/barcrawl">barcrawl</Link>
-                    </li>
-                </ul>
-                {/*end header example*/}
-                <Switch>
-                    <Route exact path="/">
-                        {/*login page*/}
-                        <p>login</p>
-                    </Route>
-                    <Route path="/randomizer">
-                        {/*bar randomizer page*/}
-                        <p>random</p>
-                    </Route>
-                    <Route path="/barcrawl">
-                        {/*barcrawl page*/}
-                        <p>barcrawl</p>
-                    </Route>
-                    <Route path="/feed">
-                        {/*news page*/}
-                        <p>feed</p>
-                    </Route>
-                    <Route path="/map">
-                        {/*map page*/}
-                        <p>map</p>
-                    </Route>
-                </Switch>
-                {/*footer*/}
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        {/*header*/}
+        {/*start header example*/}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/randomizer">randomizer</Link>
+          </li>
+          <li>
+            <Link to="/barcrawl">barcrawl</Link>
+          </li>
+        </ul>
+        {/*end header example*/}
+        <Switch>
+          <Route exact path="/">
+            {/*login page*/}
+            <p>login</p>
+          </Route>
+          <Route path="/randomizer">
+            {/*bar randomizer page*/}
+            <p>random</p>
+          </Route>
+          <Route path="/barcrawl">
+            {/*barcrawl page*/}
+            <p>barcrawl</p>
+          </Route>
+          <Route path="/feed">
+            {/*news page*/}
+            <p>feed</p>
+          </Route>
+          <Route path="/map">
+            {/*map page*/}
+            <p>map</p>
+          </Route>
+        </Switch>
+        {/*footer*/}
+      </Router>
+    );
+  }
 }
 
 export default App;
