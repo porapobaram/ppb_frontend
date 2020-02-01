27e703b556359dd175b869bfffecc227a37ddc77
Andrew Kryshtal <andrewkryshtal@MacBook-Pro-AE.local>
2020-02-02 00:14:46 +0200

    added reset css and variables files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 import App from "./App";
+import './ui/styles/reset.scss';
 
 const store = configureStore({});
 
