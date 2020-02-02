ea8dd644785d77b322068ce748d49a8f00d7db51
Nickolay <kuzmin.study@gmail.com>
2020-02-02 22:47:24 +0200

    save commit

--- src/ui/routes/ProtectedRoutes.js
+++ src/ui/routes/ProtectedRoutes.js
@@ -1,48 +1,48 @@
-import React from "react";
-import { Link, Redirect, Route, Switch } from "react-router-dom";
-import Footer from "../common/Footer";
-import NotFound from "../components/NotFound";
-import RandomizeScreenContainer from "../components/RandomizeScreen/RandomizeScreenContainer";
+import React from 'react';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
+import Footer from '../common/Footer';
+import NotFound from '../components/NotFound';
+import RandomizeScreenContainer from '../components/RandomizeScreen/RandomizeScreenContainer';
 
 const ProtectedRoutes = () => (
-  <div>
-    {/* header placement */}
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/randomizer">randomizer</Link>
-      </li>
-      <li>
-        <Link to="/barcrawl">barcrawl</Link>
-      </li>
-    </ul>
-    {/* header placement end */}
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/randomizer" />
-      </Route>
-      <Route exact path="/randomizer">
-        <RandomizeScreenContainer />
-        <p>random</p>
-      </Route>
-      <Route exact path="/barcrawl">
-        {/* barcrawl page */}
-        <p>barcrawl</p>
-      </Route>
-      <Route exact path="/feed">
-        {/* news page */}
-        <p>feed</p>
-      </Route>
-      <Route exact path="/map">
-        {/* map page */}
-        <p>map</p>
-      </Route>
-      <Route component={NotFound} />
-    </Switch>
-    <Footer />
-  </div>
+	<div>
+		{/* header placement */}
+		<ul>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/randomizer">randomizer</Link>
+			</li>
+			<li>
+				<Link to="/barcrawl">barcrawl</Link>
+			</li>
+		</ul>
+		{/* header placement end */}
+		<Switch>
+			<Route exact path="/">
+				<Redirect to="/randomizer" />
+			</Route>
+			<Route exact path="/randomizer">
+				<RandomizeScreenContainer />
+				<p>random</p>
+			</Route>
+			<Route exact path="/barcrawl">
+				{/* barcrawl page */}
+				<p>barcrawl</p>
+			</Route>
+			<Route exact path="/feed">
+				{/* news page */}
+				<p>feed</p>
+			</Route>
+			<Route exact path="/map">
+				{/* map page */}
+				<p>map</p>
+			</Route>
+			<Route component={NotFound} />
+		</Switch>
+		<Footer />
+	</div>
 );
 
 export { ProtectedRoutes };
