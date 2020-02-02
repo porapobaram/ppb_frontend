a41b8824a29e658bb88104148ca4b574d0ac6a41
NataliyaPetruk <48244809+NataliyaPetruk@users.noreply.github.com>
2020-02-02 21:22:16 +0200

    cleared notfound

--- src/ui/components/NotFound/NotFoundComponent.js
+++ src/ui/components/NotFound/NotFoundComponent.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './notFound.styles.scss';
 
 const NotFoundComponent = () => {
 	return (
-		<div className="notfound-wrapper">
-			<h1 className="notfound-title">404</h1>
-			<p className="notfound-text">page not found</p>
+		<div>
+			<h1>NOT FOUND</h1>
 		</div>
 	);
 };
