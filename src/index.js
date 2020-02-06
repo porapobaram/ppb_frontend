ed31426f3d9251ea87bca925bb41826b197d137d
Andrew Kryshtal <andrewkryshtal@MacBook-Pro-AE.local>
2020-02-06 16:20:13 +0200

    develop-team-4 fixed router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './reduxStore/store';
-// import App from './App';
-import NotFound from './ui/components/NotFound';
+import App from './App';
 import './ui/styles/reset.scss';
 
 const store = configureStore({});
 
 ReactDOM.render(
 	<Provider store={store}>
-		<NotFound />
+		<App />
 	</Provider>,
 	document.getElementById('root')
 );
