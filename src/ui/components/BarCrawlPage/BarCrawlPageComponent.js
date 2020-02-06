2f4790e668c2646892b7db966b11bf9dd44041be
NataliyaPetruk <48244809+NataliyaPetruk@users.noreply.github.com>
2020-02-06 21:30:17 +0200

    added next/prev buttons

--- src/ui/components/BarCrawlPage/BarCrawlPageComponent.js
+++ src/ui/components/BarCrawlPage/BarCrawlPageComponent.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/no-this-in-sfc */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import BarCrawlItem from './BarCrawlItem';
@@ -7,16 +9,27 @@ import { object, func } from 'prop-types';
 const propTypes = {
 	settings: object,
 	onClickHandler: func,
+	onNextArrow: func,
+	onPreviousArrow: func,
+	ref: func,
 };
 
 // eslint-disable-next-line react/prop-types
-const BarCrawlPageComponent = ({ settings, onClickHandler, Slider }) => (
+const BarCrawlPageComponent = ({ settings, onClickHandler, Slider, onNextArrow, onPreviousArrow, ref }) => (
 	<div>
-		<Slider {...settings}>
+		<Slider ref={ref} {...settings}>
 			{test.map(item => {
 				return <BarCrawlItem onClickHandler={onClickHandler} key={item.bcId} item={item} />;
 			})}
 		</Slider>
+		<div style={{ textAlign: 'center' }}>
+			<button className="button" onClick={onPreviousArrow}>
+				Previous
+			</button>
+			<button className="button" onClick={onNextArrow}>
+				Next
+			</button>
+		</div>
 	</div>
 );
 
--- src/ui/components/BarCrawlPage/BarCrawlPageContainer.js
+++ src/ui/components/BarCrawlPage/BarCrawlPageContainer.js
@@ -14,6 +14,8 @@ class BarCrawlPageContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.onClickHandler = this.onClickHandler.bind(this);
+		this.next = this.next.bind(this);
+		this.previous = this.previous.bind(this);
 	}
 
 	componentDidMount() {
@@ -25,15 +27,32 @@ class BarCrawlPageContainer extends Component {
 		console.log('test');
 	};
 
+	next() {
+		this.slider.slickNext();
+	}
+
+	previous() {
+		this.slider.slickPrev();
+	}
+
 	render() {
 		const settings = {
-			dots: true,
+			dots: false,
 			infinite: true,
 			speed: 500,
 			slidesToShow: 1,
 			slidesToScroll: 1,
 		};
-		return <BarCrawlPageComponent Slider={Slider} onClickHandler={this.onClickHandler} settings={settings} />;
+		return (
+			<BarCrawlPageComponent
+				Slider={Slider}
+				onClickHandler={this.onClickHandler}
+				settings={settings}
+				onNextArrow={this.next}
+				onPreviousArrow={this.previous}
+				ref={slider => (this.slider = slider)}
+			/>
+		);
 	}
 }
 
