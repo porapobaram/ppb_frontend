b4a159dfb745e72aa8fbd506d4336029fdb97949
Andrew Kryshtal <andrewkryshtal@MacBook-Pro-AE.local>
2020-02-08 18:13:42 +0200

    added combineReducer

---
+++ src/reduxStore/combineReducers.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import { randomBarReducer } from './bar/reducer';
+import { barCrawlReducer } from './barCrawl/reducer';
+import { barNewsReducer } from './news/reducer';
+import { connectRouter } from 'connected-react-router';
+
+export const rootReducer = history =>
+	combineReducers({
+		router: connectRouter(history),
+		randomBarReducer,
+		barCrawlReducer,
+		barNewsReducer,
+	});
