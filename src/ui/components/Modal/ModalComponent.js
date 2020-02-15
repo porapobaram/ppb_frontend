f0012e8d22bf5dbf8522c6f01fb6be8ccc9d8473
Nickolay <kuzmin.study@gmail.com>
2020-02-15 18:03:53 +0200

    add Modal`s file & start working

---
+++ src/ui/components/Modal/ModalComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ModalComponent.scss';
+
+const ModalComponent = () => {
+    return (
+        <div className="modal_wrapper">
+            <div className="modal_container">
+                <h2>ALERT</h2>
+                <p>Some info</p>
+            </div>
+        </div>
+    )
+};
+
+export default ModalComponent;
