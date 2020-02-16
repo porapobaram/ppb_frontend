d87810dfdfa6db5fc44cbaceaaade54469f1b49d
Nickolay <kuzmin.study@gmail.com>
2020-02-16 17:28:35 +0200

    ->NoteBook

--- src/ui/components/Modal/ModalContainer.js
+++ src/ui/components/Modal/ModalContainer.js
@@ -1,14 +1,14 @@
 import React, * as react from 'react';
-import ModalComponent from "./ModalComponent";
+import ModalComponent from './ModalComponent';
 
 class ModalContainer extends react.Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        return <ModalComponent />;
-    }
+	render() {
+		return <ModalComponent />;
+	}
 }
 
 export default ModalContainer;
