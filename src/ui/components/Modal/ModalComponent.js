837419c63d37c5101cbb713d5bde94fb82945bb1
Nickolay Kuzmyn <kuzmin.study@gmail.com>
2020-02-16 14:49:38 +0200

    add functions to Modal Component

--- src/ui/components/Modal/ModalComponent.js
+++ src/ui/components/Modal/ModalComponent.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './ModalComponent.scss';
 
-const ModalComponent = () => {
-    return (
-        <div className="modal_wrapper">
-            <div className="modal_container">
-                <h2>ALERT</h2>
-                <p>You drunk</p>
-            </div>
-        </div>
-    )
+const ModalComponent = ({onClickHandler1, onClickHandler2}) => {
+	return (
+		<div className="modal_wrapper">
+			<div className="modal_container">
+				<h2>ALERT</h2>
+				<p>You drunk</p>
+				<div className="modal_buttons">
+					<button onClick={onClickHandler1}>Continue?</button>
+					<button onClick={onClickHandler2}>Continue!</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ModalComponent;
--- src/ui/components/Modal/ModalContainer.js
+++ src/ui/components/Modal/ModalContainer.js
@@ -1,14 +1,24 @@
 import React, * as react from 'react';
-import ModalComponent from "./ModalComponent";
+import ModalComponent from './ModalComponent';
 
 class ModalContainer extends react.Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+		this.onClickHandler1 = this.onClickHandler1.bind(this);
+		this.onClickHandler2 = this.onClickHandler2.bind(this);
+	}
 
-    render() {
-        return <ModalComponent />;
-    }
+	onClickHandler1 = () => {
+		console.log('Click_1');
+	};
+
+	onClickHandler2 = () => {
+		console.log('Click_2');
+	};
+
+	render() {
+		return <ModalComponent onClickHandler1={this.onClickHandler1} onClickHandler2={this.onClickHandler2} />;
+	}
 }
 
 export default ModalContainer;
