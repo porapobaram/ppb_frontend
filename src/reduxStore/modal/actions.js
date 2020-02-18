09a2e9458f60c6c25f0778b1f105cf1f80b71d34
Nickolay Kuzmyn <kuzmin.study@gmail.com>
2020-02-18 22:45:34 +0200

    save commit

--- src/reduxStore/modal/actions.js
+++ src/reduxStore/modal/actions.js
@@ -1,8 +1,8 @@
 import * as actionTypes from './actionTypes';
 
-export const openModal = (modalType) => dispatch => {
-	dispatch({ type: actionTypes.OPEN_MODAL, payload: {modalType: modalType, modalProps: ''} });
+export const openModal = modalType => dispatch => {
+	dispatch({ type: actionTypes.OPEN_MODAL, payload: { modalType, modalProps: '' } });
 };
 export const closeModal = () => dispatch => {
-	dispatch({ type: actionTypes.CLOSE_MODAL, payload: {modalType: '', modalProps: ''} });
+	dispatch({ type: actionTypes.CLOSE_MODAL, payload: { modalType: '', modalProps: '' } });
 };
