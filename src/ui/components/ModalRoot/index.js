10a9d5b054d8666cbb109c2abcbdf380c4542ff2
Nickolay <kuzmin.study@gmail.com>
2020-02-23 19:16:26 +0200

    fix some troubles

--- src/ui/components/ModalRoot/index.js
+++ src/ui/components/ModalRoot/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ToBarModalComponent from '../Modals/ToBarModal';
 import ImHereModalComponent from '../Modals/ImHereModal/ImHereModalComponent';
-import BonusBarModalComponent from "../Modals/BonusBarModal/BonusBarModalComponent";
+import BonusBarModalComponent from '../Modals/BonusBarModal/BonusBarModalComponent';
 
 class ModalRoot extends Component {
 	render() {
