2f008c48fe654b6cd111b1c3383118b536619820
NataliyaPetruk <48244809+NataliyaPetruk@users.noreply.github.com>
2020-02-27 21:17:00 +0200

    added new styles

--- src/ui/components/Modals/CommentsModal/CommentsModalComponent.js
+++ src/ui/components/Modals/CommentsModal/CommentsModalComponent.js
@@ -1,7 +1,47 @@
+// import React from 'react';
+// import CommentsModalItem from './CommentsModalItem';
+// import './comments.styles.scss';
+// import { func, arr } from 'prop-types';
+// import { data } from './data';
+
+// const propTypes = {
+// 	onClickHandler: func,
+// 	comments: arr,
+// };
+
+// const CommentsModalComponent = ({ onClickHandler, comments }) => {
+// 	console.log(comments);
+// 	return (
+// 		<div className="modal-wrapper">
+// 			<div className="modal-header">
+// 				<p className="modal-title">Комментарии</p>
+// 				<div className="closed-btn-wrapper">
+// 					<div className="closed-btn" onClick={onClickHandler} />
+// 				</div>
+// 			</div>
+// 			<div className="modal-container">
+// 				{data && data.map(item => <CommentsModalItem item={item} key={item.userId} />)}
+// 			</div>
+// 			<form className="modal-form">
+// 				{/* <input className="modal-input" type="text" label="label" /> */}
+// 				<textarea rows="3" className="modal-input" placeholder="Оставить комментарий..." />
+// 				<button className="add-btn" type="button">
+// 					Добавить
+// 				</button>
+// 			</form>
+// 		</div>
+// 	);
+// };
+
+// CommentsModalComponent.propTypes = propTypes;
+
+// export { CommentsModalComponent };
+
 import React from 'react';
 import CommentsModalItem from './CommentsModalItem';
 import './comments.styles.scss';
 import { func, arr } from 'prop-types';
+import { data } from './data';
 
 const propTypes = {
 	onClickHandler: func,
@@ -11,23 +51,25 @@ const propTypes = {
 const CommentsModalComponent = ({ onClickHandler, comments }) => {
 	console.log(comments);
 	return (
-		<div className="modal-wrapper">
-			<div className="modal-header">
-				<p className="modal-title">Комментарии</p>
-				<div className="closed-btn-wrapper">
-					<div className="closed-btn" onClick={onClickHandler} />
+		<div className="commentsModal-wrapper">
+			<div className="commentsModal-container">
+				<div className="commentsModal-header">
+					<p className="commentsModal-header-title">Комментарии</p>
+					<div className="commentsModal-closed-btn-wrapper">
+						<div className="commentsModal-closed-btn" onClick={onClickHandler} />
+					</div>
 				</div>
+				<div className="commentsModalItem-wrapper">
+					{data && data.map(item => <CommentsModalItem item={item} key={item.userId} />)}
+				</div>
+				<form className="commentsModal-form">
+					{/* <input className="modal-input" type="text" label="label" /> */}
+					<textarea rows="3" className="commentsModal-textarea" placeholder="Оставить комментарий..." />
+					<button className="commentsModal-add-btn" type="button">
+						<span>+</span>
+					</button>
+				</form>
 			</div>
-			<div className="modal-container">
-				{comments && comments.map(item => <CommentsModalItem item={item} key={item.userId} />)}
-			</div>
-			<form className="modal-form">
-				<input className="modal-input" type="text" label="label" />
-				{/* <div className="add-btn" /> */}
-				{/* <button className="add-btn" type="submit">
-					Add
-				</button> */}
-			</form>
 		</div>
 	);
 };
--- src/ui/components/Modals/CommentsModal/CommentsModalItem/index.js
+++ src/ui/components/Modals/CommentsModal/CommentsModalItem/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './commentsItem.styles.scss';
 
 const CommentsModalItem = ({ item }) => {
-	const { userName, userComment } = item;
+	const { userName, userComment, userPic } = item;
 	return (
 		<div className="modalItem-container">
-			<h2>{userName}</h2>
-			<p>{userComment}</p>
+			<div className="modalItem-userPic-container">
+				<img src={userPic} alt="userPic" className="modalItem-userPic" />
+			</div>
+			<div className="modalItem-userText-container">
+				<h2 className="modalItem-userName">{userName}</h2>
+				<p className="modalItem-userComment">{userComment}</p>
+			</div>
 		</div>
 	);
 };
